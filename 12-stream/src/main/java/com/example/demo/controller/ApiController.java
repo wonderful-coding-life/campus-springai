093b47a8234997e948b234fe4138c8418256c426
spiderman <spiderman@xhift.co.kr>
2026-07-15 22:47:17 +0900

    added 12-stream

---
+++ 12-stream/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import tools.jackson.databind.ObjectMapper;
+
+@RestController
+public class ApiController {
+    @Autowired
+    private ChatClient chatClient;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping(value="/chats", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getChats(@RequestParam("message") String message) {
+        return chatClient.prompt()
+                .user(message)
+                .stream()
+                .content()
+                .map(objectMapper::writeValueAsString);
+    }
+
+    @PostMapping(value="/chats", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> postChats(@RequestBody String message) {
+        return chatClient.prompt()
+                .user(message)
+                .stream()
+                .content()
+                .map(objectMapper::writeValueAsString);
+    }
+}
