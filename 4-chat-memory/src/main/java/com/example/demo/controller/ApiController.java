90b75856495acf2105f419386d7dbede5df29d61
spiderman <spiderman@xhift.co.kr>
2026-08-01 06:25:12 +0900

    updated 4-chat-memory

--- 4-chat-memory/src/main/java/com/example/demo/controller/ApiController.java
+++ 4-chat-memory/src/main/java/com/example/demo/controller/ApiController.java
@@ -6,10 +6,7 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApiController {
@@ -19,7 +16,7 @@ public class ApiController {
     @Autowired
     private ChatMemory chatMemory;
 
-    @PostMapping("/chats/model")
+    @PostMapping("/api/v1/chats")
     public String postChatModel(@RequestBody String message, @RequestParam("id") String id) {
         chatMemory.add(id, UserMessage.builder().text(message).build());
         var response = chatModel.call(Prompt.builder().messages(chatMemory.get(id)).build());
@@ -30,12 +27,17 @@ public class ApiController {
     @Autowired
     private ChatClient chatClient;
 
-    @PostMapping("/chats/client")
+    @PostMapping(value={"/api/v2/chats", "/chats"})
     public String postChatClient(@RequestBody String message, @RequestParam("id") String id) {
         return chatClient.prompt()
                 //.advisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, id))
                 .user(message)
                 .call().content();
+    }
+
+    @DeleteMapping("/chats")
+    public void deleteChats(@RequestParam("id") String id) {
+        chatMemory.clear(id);
     }
 }
